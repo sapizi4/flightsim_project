8b7cfc475504c8a02d45e11ca515fecb56517461
MaorBokobza <51862468+MaorBokobza@users.noreply.github.com>
2020-01-30 17:41:41 +0200

    Update AssignCommand.cpp

--- AssignCommand.cpp
+++ AssignCommand.cpp
@@ -1,9 +1,13 @@
+#include <mutex>
 #include "AssignCommand.h"
 #include "DataReaderServer.h"
+#include "SymbolTable.h"
+
 
 AssignCommand::AssignCommand( int &index) : index(index){}
 
 void AssignCommand::execute(const vector<string> &cur_lex) {
+    mutex globalMutex;
     Shuntingyard shuntingyard;
     string key = cur_lex[index];
     if(key=="{"){
@@ -11,7 +15,7 @@ void AssignCommand::execute(const vector<string> &cur_lex) {
         index++;
     }
     index += 2; // skip key and '='
-    double val = shuntingyard.algorithm(shuntingyard.extract_string(cur_lex[index]));
+    double val = Shuntingyard::algorithm(Shuntingyard::extract_string(cur_lex[index]));
     globalMutex.lock();
     SymbolTable::instance()->setValue(key,val);
     if (BindingTable::instance()->getValue(key)[0] == '/'){
