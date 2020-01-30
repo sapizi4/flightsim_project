6b5f5c31a505058179fba552ae4834444dc19b36
MaorBokobza <51862468+MaorBokobza@users.noreply.github.com>
2020-01-30 20:10:20 +0200

    MileStone2 main

--- main.cpp
+++ main.cpp
@@ -1,39 +1,26 @@
-#include <iostream>
-#include <strings.h>
-#include "Tables/BindTable.h"
-#include "Tables/SymbolTable.h"
-#include "Tables/PathsTable.h"
-#include "list"
-#include "Interpeter/Lexer.h"
-#include "Commands/ConnectCommand.h"
-#include "Interpeter/Parser.h"
-#include "Commands/OpenServerCommand.h"
-#include <stack>
-#include "CacheManager.h"
-#include "FileCacheManager.h"
-#include "StringReverser.h"
-#include "MySerialServer.h"
-#include "ClientHandler.h"
-#include "MyTestClientHandler.h"
-#include "MatrixSearchable.h"
-#include "Searcher.h"
-#include "MatrixSolver.h"
-#include "BestFirstSearch.h"
-#include "A_Star.h"
+//
+// Created by maor on 29/01/2020.
+//
+
 #include "MyParallelServer.h"
-#include "Main.h"
+#include "Searcher.h"
+#include "A_Star.h"
+#include "CacheManager.h"
+#include "ClientHandler.h"
+#include "DFS.h"
+#include "BFS.h"
+#include "FileCacheManager.h"
 
-#include <iostream>
-using namespace std;
-using namespace boot;
+int main(int argc, char *argv[]) {
 
-BindTable *BindTable::s_instance = 0;
-SymbolTable *SymbolTable::s_instance = 0;
-PathsTable *PathsTable::s_instance = 0;
+    int port = stoi(argv[1]);
+    MyParallelServer myParallelServer;
+    Searcher<pair<int, int>, string> *searcherInterface = new DFS<pair<int, int>, string>;
+    GraphSearcher<Searchable<pair<int, int>> *, string, pair<int, int>> *solver
+            = new GraphSearcher<Searchable<pair<int, int>> *, string, pair<int, int>>(searcherInterface);
+    CacheManager<string, string> *cacheManager = new FileCacheManager;
+    ClientHandler *clientHandler = new ClientHandler(solver, cacheManager);
+    myParallelServer.open(port, clientHandler);
 
-
-int main(int argc, char **argv) {
-    Main* run = new Main();
-    run->main(argc, argv);
-    delete (run);
+    return 0;
 }
