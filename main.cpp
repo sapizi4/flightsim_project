ff28024d0ddc61d30d927026b25e3503aa94490e
MaorBokobza <51862468+MaorBokobza@users.noreply.github.com>
2020-01-30 20:14:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 //
 // Created by maor on 29/01/2020.
 //
-
 #include "MyParallelServer.h"
 #include "Searcher.h"
 #include "A_Star.h"
@@ -14,13 +13,14 @@
 int main(int argc, char *argv[]) {
 
     int port = stoi(argv[1]);
-    MyParallelServer myParallelServer;
-    Searcher<pair<int, int>, string> *searcherInterface = new DFS<pair<int, int>, string>;
-    GraphSearcher<Searchable<pair<int, int>> *, string, pair<int, int>> *solver
-            = new GraphSearcher<Searchable<pair<int, int>> *, string, pair<int, int>>(searcherInterface);
-    CacheManager<string, string> *cacheManager = new FileCacheManager;
-    ClientHandler *clientHandler = new ClientHandler(solver, cacheManager);
+    MyParallelServer myParallelServer
+    Searcher<int>, string> *searcherInterface = new DFS< int>, string>;
+    GraphSearcher<Searchable<int>> *, string, int> *solver
+            = new GraphSearcher<Searchable< int>> *, string, int>(searcherInterface);
+    CacheManager<string> *cacheManager = new FileCacheManager;
+    ClientHandler *clientHandler = new ClientHandler(sockFd);
     myParallelServer.open(port, clientHandler);
 
     return 0;
 }
+
