52e70a73724a2deb3fec0ad87552cd30a963c766
MaorBokobza <51862468+MaorBokobza@users.noreply.github.com>
2020-01-30 18:17:19 +0200

    Update MyPriorityQueue.h

--- MyPriorityQueue.h
+++ MyPriorityQueue.h
@@ -10,7 +10,7 @@
 #include <iostream>
 #include "State.h"
 #include "Comparator.h"
-//extend of original priority queue,  adding functionallity
+//extend of original priority queue,  adding functionality
 using namespace std;
 
 template<class T>
@@ -18,22 +18,23 @@ class MyPriorityQueue:public priority_queue<T,vector<T>,Comparator<T>>{
 
 public:
     T remove(const T &value) {
-    auto it = std::find(this->c.begin(), this->c.end(), value);
-    if (it != this->c.end()) {
-        T st = *it;
-        this->c.erase(it);
-        this->make_heaps();
-        return st;
+        auto it = std::find(this->c.begin(), this->c.end(), value);
+        if (it != this->c.end()) {
+            T st = *it;
+            this->c.erase(it);
+            this->make_heaps();
+            return st;
+        }
     }
-}
     bool atPriorityQueue(const T &value) {
-    auto it = std::find(this->c.begin(), this->c.end(), value);
-    return it != this->c.end();
-}
+        auto it = std::find(this->c.begin(), this->c.end(), value);
+        return it != this->c.end();
+    }
     inline void make_heaps(){
-    make_heap(this->c.begin(), this->c.end(), this->comp);
-}
-    ~MyPriorityQueue(){};
+        make_heap(this->c.begin(), this->c.end(), this->comp);
+    }
+    ~MyPriorityQueue()= default;;
 
 };
+
 #endif //FLIGHTSIM_PROJECT_MYPRIORITYQUEUE_H
